e39215ee3b8fd948667e0da4163bfd1d6b79f698
Jiucheng Zang <zangjiucheng@gmail.com>
2024-11-24 21:25:45 -0500

    Add .gitignore, LICENSE, and update-upstream script

---
+++ update-upstream.sh
@@ -0,0 +1,4 @@
+git subtree pull --prefix=dwm https://git.suckless.org/dwm master --squash
+git subtree pull --prefix=dmenu https://git.suckless.org/dmenu master --squash
+git subtree pull --prefix=st https://git.suckless.org/st master --squash
+
